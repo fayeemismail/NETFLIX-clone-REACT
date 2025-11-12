a89c16ba4cc1c42f4451559c7d97082070c8f8a1
fayeemismail <faheemmuhammed703@gmail.com>
2025-11-12 15:52:54 +0530

    Fix: separate firebase file

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
-import React, { useEffect } from 'react'
-import Home from './pages/Home/Home'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Player from './pages/Player/Player'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from './firebase'
-import { ToastContainer, toast } from 'react-toastify';
+import React, { useEffect } from 'react';
+import Home from './pages/Home/Home';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Player from './pages/Player/Player';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  useEffect(()=> {
-    onAuthStateChanged(auth, async (user)=> {
-      if(user){
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
         navigate('/');
-        console.log('logged in')
-      }else{
+        console.log('logged in');
+      } else {
         navigate('/login');
-        console.log('logged Out')
+        console.log('logged Out');
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <div>
-
-      <ToastContainer theme='dark' />
+      <ToastContainer theme="dark" />
       <Routes>
-
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/player/:id' element={<Player />} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
